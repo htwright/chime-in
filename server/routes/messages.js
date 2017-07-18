0673d9ba0a87acc205f496ed47b7dc79c84dee44
Zach Williams <saumanahaii@gmail.com>
2017-07-18 16:12:18 -0400

    Got the message get function working

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -12,21 +12,29 @@ mRoutes.get('/', (req, res) => {
   res.status(200).json({ message: 'The raw endpoint.  Maybe try using the actual points?' });
 });
 
-mRoutes.post("/sendMessage",(req,res,next)=>{
+mRoutes.post("/send",(req,res,next)=>{
 
   client.messages.create({
-  to:req.body.phone,
-  body: req.body.message,
-  from: "+12409863225",
-  statusCallback: "http://chime-in.herokuapp.com/messages"
-}).then((msgID)=>{
-  //console.log(msgID)
-  res.status(200).json({
-    message: 'Sent the message "' + req.body.message + '", good job!',
-  });
-}).catch((err,msg)=>{
-  console.log(err);
-})
+    to:req.body.phone,
+    body: req.body.message,
+    from: "+12409863225",
+    statusCallback: "http://chime-in.herokuapp.com/messages"
+  }).then((msgID)=>{
+    console.log(msgID)
+    res.status(200).json({
+      message: 'Sent the message "' + req.body.message + '", good job!',
+      messageID: msgID.sid
+    });
+  }).catch((err,msg)=>{
+    console.log(err);
+  })
+});
+
+mRoutes.get("/get/:messageID", (req,res,next)=>{
+  client.messages(req.params.messageID).fetch().then(msg=>{
+    console.log(msg.body)
+    res.status(200).json({message:msg.body});
+  })
 })
 
 module.exports = mRoutes;
