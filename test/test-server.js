107cc58d85752a474e0efac0ad03a6619df7ba04
tachyla <tachylad.murray@gmail.com>
2017-07-19 08:45:59 -0700

    GET using /api/hello endpoint passing

--- test/test-server.js
+++ test/test-server.js
@@ -5,7 +5,6 @@ let client = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const jsonParser = require('body-parser').json();
-// const faker = require('faker');
 const should = chai.should();
 
 const { app, runServer, closeServer } = require('../server/index');
@@ -32,18 +31,14 @@ describe('Backend unit tests', function() {
   //   1. make request to `/api/hello`
   //   2. check response object keys
   it('should list all messages on GET', function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
     return chai.request(app)
       .get('/api/hello')
       .then(function(res) {
         res.should.be.ok;
-        //res.should.have.status(200);
-        //res.should.be.json;
-        //res.body.should.be.a('array');
-       //res.body.length.should.be.at.least(1);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('string');
+        res.body.length.should.be.at.least(1);
         // each item should be an object with key/value pairs
         //const expectedKeys = ['id', 'name', 'checked'];
         //res.body.forEach(function(item) {
