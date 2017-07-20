497d4d0839b673944d3e1b90d358a76399ef1ff7
tachyla <tachylad.murray@gmail.com>
2017-07-20 09:04:31 -0700

    removed message-controlLabel

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import Form from 'react-bootstrap/lib/Form';
 import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+
+
 import ListGroup from 'react-bootstrap/lib/ListGroup';
 import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
+import InputGroup from 'react-bootstrap/lib/InputGroup';
+
 
 import './App.css';
 
@@ -12,22 +18,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className="App-header">
           <Button className="logout">LogOut</Button>            
           <h2>Chime-In</h2>
         </div>
           <div>
             <h1>Create a Message</h1>
-            <Form className="input-field">
+            <Form inline>
+              <FormGroup controlId="formInlinePassword">
+                <ControlLabel>Phone #</ControlLabel>
+                {' '}
+                <FormControl type="text" placeholder="US Phone Number"/>
+                </FormGroup>
+                {' '}
+              <FormGroup controlId="formInlineName">
+                <ControlLabel></ControlLabel>
+                {' '}
+                <FormControl type="text" placeholder="Enter message here" />
+              </FormGroup>
+              {' '}
+                <Button type="submit">Enter</Button>
+            </Form>
+            {/* <Form className="input-field">
               <input placeholder="US Phone #" />
               <input placeholder="Message" />
                 <Button>Send Message</Button>
-            </Form>
-            
+            </Form> */}
           </div>
 
           <div className='column' id="admin-col-one">
-
             <h1>Questions</h1>
             <ListGroup>
               <ListGroupItem href="#linkA">Bob ate what?</ListGroupItem>
@@ -38,10 +58,8 @@ class App extends Component {
 
           <div className='column' id="admin-col-two">
             <h1>Users</h1>
-
             {/* function alertClicked() {
               alert('You clicked the third ListGroupItem');} */}
-
             <ListGroup>
               <ListGroupItem href="#link1">Joe</ListGroupItem>
               <ListGroupItem href="#link2">Jane</ListGroupItem>
