a52138af06743d6c4acdedff2922f7e7d63eb5ec
Zach Williams <saumanahaii@gmail.com>
2017-07-26 12:41:17 -0400

    Squashed a logging in bug

--- server/functions/auth.js
+++ server/functions/auth.js
@@ -12,10 +12,12 @@ const knex = require('knex')({
 Auth = (req,res,next) => {
   console.log("Hit auth endpoint.");
   let authDetails = req.get("Authorization").split(" ");
+  let userNumber = null;
   knex("users").select().where({name: authDetails[0]}).then(result=>{
     //console.log(result);
     if(result.length>0){
       //console.log("user exists!");
+      userNumber = result[0].id
       return result;
     }else{
       //console.log("login failed.");
@@ -23,12 +25,13 @@ Auth = (req,res,next) => {
     }
   }).then(result=>{
     if(result){
-      return knex("tokens").select().where({tokenstring:authDetails[1]})
+      return knex("tokens").select().where({userid: userNumber,tokenstring:authDetails[1]})
     }else{
       return null;
     }
   }).then(result=>{
-    if(result){
+    console.log(result)
+    if(result.length>0){
       console.log("logged in successfully.");
       next();
     }else{
