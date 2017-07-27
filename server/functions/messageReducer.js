6eba199b9b0822fa5055fdcde0f89bdcb65719dd
Zach Williams <saumanahaii@gmail.com>
2017-07-27 00:15:04 -0400

    various bug smashing, making the app flexible enough to use different numbers and different URLs.  Probably more still to go.

--- server/functions/messageReducer.js
+++ server/functions/messageReducer.js
@@ -1,9 +1,12 @@
 //purpose: middleware that runs whenever a message comes in.
 const conf = require("../config");
-
+const Messaging = require("./messages");
+const Message = new Messaging();
 const knex = require('./knex')();
 
 MessageReducer = (req,res,next) =>{
+  console.log("hit the message reducer!")
+  Message.send(req,1);
   console.log(req);
 }
 
