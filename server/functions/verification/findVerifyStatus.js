64fc81ade8c362b6e51260067a05cc3533a9eb85
Zach Williams <saumanahaii@gmail.com>
2017-07-31 16:08:01 -0400

    still working on user verification.

--- server/functions/verification/findVerifyStatus.js
+++ server/functions/verification/findVerifyStatus.js
@@ -3,7 +3,7 @@ const knex = require('../knex')();
 
 const findVerifyStatus = (userId,adminId) => {
   // console.log(`user id: ${userId}, admin id: ${adminId}`)
-  // return knex('verify').where({userid:userId,adminid:adminId})
+  return knex('verify').where({userid:userId,adminid:adminId})
     .then(data => {
       // console.log("data in findVerifyStatus is................");
       // console.log(data)
