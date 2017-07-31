11ff2bf9fee86694de64f833fbb2de0be674a479
Zach Williams <saumanahaii@gmail.com>
2017-07-31 18:46:33 -0400

    still working on user verification.

--- server/functions/messageReducer.js
+++ server/functions/messageReducer.js
@@ -23,15 +23,13 @@ MessageReducer = (req,res,next) =>{
       {
         findVerifyStatus(user.id,currentQuestion[0].admin).then(result=>{
           //now do stuff depending on whether the user verified that admin.
-          console.log(".....................................................................");
-          console.log(result[0])
+
           if(!Array.isArray(result)) result = [result]
           else if(result===[]) result = null;
+          console.log(".....................................................................");
+          console.log(result)
           if (result !== null){
             //cast result into array if it isn't one
-
-
-
             if(result.length>0){
               if(result[0].status === "verified"){
                 //do normal account stuff that's broken out into its own function.
