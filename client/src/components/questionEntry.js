6365b497df96694a303220e3972509af8f2d5bac
pham-thien <pham.thien@outlook.com>
2017-07-31 18:20:42 -0400

    adding oauth

--- client/src/components/questionEntry.js
+++ client/src/components/questionEntry.js
@@ -6,39 +6,10 @@ import {fetchQuestion} from '../actions/action';
 
 
 export class QuestionEntry extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     questions: []
-  //   }
-  //   //this.getQuestions = this.getQuestions.bind(this);
-  // }
 
   componentDidMount(){
-  //  let q = this.props.dispatch(fetchQuestion()).then(res=>{
-  //   console.log(res);
-  //   this.setState({questions: res});
-  //   console.log(this.state);
-  //  });
-  this.props.dispatch(fetchQuestion());
-
+    this.props.dispatch(fetchQuestion());
   }
-
-  //get questionsList prop & maps over each question as a listGroupItem
-  //  makeQuestions(){
-  //    console.log("its working!");
-  //  }
-  //     const questionsList =  this.props.questions.map((question, index) => {
-  //       console.log(question);
-  //       return (
-  //         <ListGroupItem> {question} </ListGroupItem>
-  //       )
-  //     })
-  //     console.log(questionsList);
-  //     return questionsList;
-  //   })
-
-  // }
 
   render() {
         const questionsList = this.props.questions.map((question, index) => {
