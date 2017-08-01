7ef652a3287dd94daaf22b7cdb1c322c6aa19471
Zach Williams <saumanahaii@gmail.com>
2017-07-31 22:08:56 -0400

    still working on user verification.

--- server/functions/addQuestionResponse.js
+++ server/functions/addQuestionResponse.js
@@ -3,6 +3,7 @@ const conf = require("../config");
 const knex = require('./knex')();
 
 let addquestionResponse = (id, response) => {
+  console.log(id);
   return knex('questions').where({id:id}).returning("responses").then(data=>{
     console.log(data)
   })
