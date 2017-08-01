f5205b0a21256f8f188889a5c566407907eb94ea
tachyla <tachylad.murray@gmail.com>
2017-08-01 09:20:53 -0700

    moved table html to its own questionsTable component

--- client/src/components/questionEntry.js
+++ client/src/components/questionEntry.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {fetchQuestion} from '../actions/action';
-import QuestionsTable from './questionsTable';
+// import QuestionsTable from './questionsTable';
 import Chart from './Chart';
 import Panel from 'react-bootstrap/lib/Panel';
 import Accordion from 'react-bootstrap/lib/Accordion';
@@ -23,25 +23,9 @@ export class QuestionEntry extends Component {
         console.log(question);
         console.log(question.question);
         return (
-            <Panel header={question.question} eventKey={key} bsStyle="info"></Panel>
-            )
+          <Panel header={question.question} eventKey={key} bsStyle="info"></Panel>
+        )
 
-              // <Table
-              //   rowsCount={2}
-              //   rowHeight={50}
-              //   headerHeight={50}
-              //   width={900}
-              //   height={200}>
-              //   <Column cell={<Cell>{question.id}</Cell>}
-              //           header={<Cell>Question ID</Cell>}
-              //           width={300}/>
-              //   <Column cell={<Cell>{question.users}</Cell>}
-              //           header={<Cell>User ID</Cell>}
-              //           width={300}/>
-              //   <Column cell={<Cell>{question.responses}</Cell>}
-              //           header={<Cell>Response</Cell>}
-              //           width={300}/>
-              // </Table>
         })
         return (
           <div className="questionXYZ">
