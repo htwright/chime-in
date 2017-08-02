4f1bc0c2b9ee6d018551778ad1aea705160118d8
tachyla <tachylad.murray@gmail.com>
2017-08-01 19:10:38 -0700

    table displays state

--- client/src/components/questionsTable.js
+++ client/src/components/questionsTable.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import PanelGroup from 'react-bootstrap/lib/PanelGroup';
 import {Table, Column, Cell} from 'fixed-data-table';
+import 'fixed-data-table/dist/fixed-data-table.min.css';
+ 
 
 export class QuestionsTable extends Component {
     constructor(props){
@@ -9,31 +10,27 @@ export class QuestionsTable extends Component {
     }
     
         render() {
-            const questionDetails = this.props.questions.map((question, index) => {
-                return(
-                <PanelGroup key={index}>  
-                    <Column header={<Cell>Question ID</Cell>} cell={<Cell>{question.id}</Cell>} width={300}/>
-                    <Column header={<Cell>Users</Cell>} cell={<Cell>{question.users}</Cell>} width={300}/>
-                    <Column header={<Cell>Responses</Cell>} cell={<Cell>{question.responses}</Cell>} width={300}/>   
-                </PanelGroup>    
-                );
-        });
-          return (
-            <Table className="table"
-                rowsCount={2}
-                rowHeight={50}
-                headerHeight={50}
-                width={900}
-                height={100}>
-                {questionDetails}
-            </Table> 
-           );
-  } 
+            return (
+                <Table className="table"
+                    rowsCount={3}
+                    rowHeight={100}
+                    headerHeight={100}
+                    width={900}
+                    height={300}>
+                    <Column  cell={<Cell>{}</Cell>}
+                             header={<Cell>Question ID</Cell>} 
+                             width={300}/>
+
+                    <Column header={<Cell>Users</Cell>} cell={<Cell>{}</Cell>} width={300}/>
+                    <Column header={<Cell>Responses</Cell>} cell={<Cell>{}</Cell>} width={300}/>
+                </Table> 
+           )
+        } 
 }
 
 export const mapStateToProps = (state, ownProps) => ({
     questions: state.questions,
-    name: ownProps.name
+    data: ownProps.data
 });
 
 export default connect(mapStateToProps)(QuestionsTable);
